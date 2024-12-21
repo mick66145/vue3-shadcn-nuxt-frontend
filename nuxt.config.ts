71372c40d14b3e0ea71e41a2105e9e2edaaa6ea1
ming <mick66145@gmail.com>
2024-12-21 10:39:43 +0800

    chore: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-quasar-ui',
+    'dayjs-nuxt',
+    "nuxt-lodash",
+    'nuxt-swiper',
+    '@nuxtjs/i18n'
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  css: ['./assets/styles/index.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+        }
+      }
+    }
+  }
 })
