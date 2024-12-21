f2bad6755f1d1b8affe5abf105d5ed33935178ce
ming <mick66145@gmail.com>
2024-12-21 21:06:21 +0800

    feat: 同步composables

--- composables/useAssetsFile.js
+++ composables/useAssetsFile.js
@@ -1,7 +1,7 @@
 
-export default function useAssetsFile () {
+export default function useAssetsFile() {
   const getImageFile = (url) => {
-    const path = `/src/assets/images/${url}`
+    const path = `/assets/images/${url}`
     const modules = import.meta.glob('@/assets/images/**/*.{png,svg,jpg,jpeg}', { eager: true })
     return modules[path].default
   }
