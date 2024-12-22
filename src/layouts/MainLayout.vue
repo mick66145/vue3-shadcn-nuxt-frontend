0ede550b2c8a82f6efc01e7ee6dfaae5372af53f
ming <mick66145@gmail.com>
2024-12-22 15:05:23 +0800

    feat: layouts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <nuxt-page />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+
+export default defineComponent({
+  setup() {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="postcss" scoped></style>
