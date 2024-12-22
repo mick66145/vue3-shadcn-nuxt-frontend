93fb5ac86918eccf0bf5db85b3daa4ec96029b69
ming <mick66145@gmail.com>
2024-12-22 17:35:59 +0800

    feat: auth 新增如果是登入狀態 導向個人首頁

--- src/middleware/auth.global.js
+++ src/middleware/auth.global.js
@@ -5,6 +5,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const store = useUser()
     const { resetStore } = useLogout()
 
+    const path = ['/entry/login', '/entry/register']
+    if (path.includes(to.path) && store.isLogin) {
+        return navigateTo('/member-center')
+    }
+
     if (to.meta.auth && !store.isLogin) {
         resetStore()
         return navigateTo('/entry/login', { params: to.params, query: { redirect: to.fullPath } })
