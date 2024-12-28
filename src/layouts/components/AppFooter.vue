c351fbecede8f340230c6cc8f6d551993eda68c9
ming <mick66145@gmail.com>
2024-12-28 22:33:25 +0800

    feat: AppFooter、AppHeader

---
+++ src/layouts/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-black text-white w-full fixed bottom-0 left-0 py-4">
+    <div class="flex justify-center items-center">
+      <p class="text-sm">© 2024 Ming All Rights Reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+export default defineComponent({
+  components: {
+  },
+  setup() {
+  },
+})
+</script>
+
+<style lang="postcss" scoped></style>
