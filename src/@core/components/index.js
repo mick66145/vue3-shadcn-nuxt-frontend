77740e10c2f68880ca8913e20b6f78a53ef6ff38
ming <mick66145@gmail.com>
2025-01-01 18:38:22 +0800

    feat: badge components

--- src/@core/components/index.js
+++ src/@core/components/index.js
@@ -8,15 +8,19 @@ import {
   CardHeader,
   CardTitle,
 } from '@core/components/ui/card'
+import { Badge } from '@core/components/ui/badge'
 
 export default {
   install(app) {
     app.component('ContainerBlock', ContainerBlock)
+
+    //ui元件
     app.component('Card', Card)
     app.component('CardContent', CardContent)
     app.component('CardDescription', CardDescription)
     app.component('CardFooter', CardFooter)
     app.component('CardHeader', CardHeader)
     app.component('CardTitle', CardTitle)
+    app.component('Badge', Badge)
   },
 }
