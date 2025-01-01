d9a46b38ec6a8e217546d1b624dcc2c5e8405773
ming <mick66145@gmail.com>
2025-01-01 18:16:34 +0800

    feat: card components

--- src/@core/components/index.js
+++ src/@core/components/index.js
@@ -1,7 +1,22 @@
 import ContainerBlock from './block/ContainerBlock.vue'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@core/components/ui/card'
+
 export default {
   install(app) {
     app.component('ContainerBlock', ContainerBlock)
+    app.component('Card', Card)
+    app.component('CardContent', CardContent)
+    app.component('CardDescription', CardDescription)
+    app.component('CardFooter', CardFooter)
+    app.component('CardHeader', CardHeader)
+    app.component('CardTitle', CardTitle)
   },
 }
