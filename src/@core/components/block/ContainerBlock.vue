2c439459e36ee0bbd536deba2bc89bc89cf86f1a
ming <mick66145@gmail.com>
2025-01-01 17:01:01 +0800

    feat: ContainerBlock

---
+++ src/@core/components/block/ContainerBlock.vue
@@ -0,0 +1,108 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue-demi'
+export default defineComponent({
+  setup () {
+    return {
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+ @import "@/assets/styles/abstracts/variables.scss";
+$container-padding: 20px;
+.container,
+.container-sm,
+.container-md,
+.container-lg,
+.container-xl {
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 15px;
+  padding-left: 15px;
+  width: 100%;
+}
+
+@media (max-width: $breakpoint-xs-max) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg,
+  .container-xl {
+    max-width: 100%;
+  }
+}
+
+@media (min-width: $breakpoint-sm-min) {
+  .container,
+  .container-sm {
+    max-width: $breakpoint-xs - $container-padding;
+  }
+}
+
+@media (min-width: $breakpoint-md-min) {
+  .container,
+  .container-sm,
+  .container-md {
+    max-width: $breakpoint-sm - $container-padding;
+  }
+}
+
+@media (min-width: $breakpoint-lg-min) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg {
+    max-width: $breakpoint-md - $container-padding;
+  }
+}
+
+@media (min-width: $breakpoint-xl-min) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg,
+  .container-xl {
+    max-width: $breakpoint-lg - $container-padding;
+  }
+}
+
+@media (min-width: $breakpoint-2xl-min) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg,
+  .container-xl,
+  .container-xxl {
+    max-width: $breakpoint-xl - $container-padding;
+  }
+}
+
+@media screen and (width >=1600px) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg,
+  .container-xl,
+  .container-xxl {
+    max-width: 1560px;
+  }
+}
+
+@media screen and (width >=1800px) {
+  .container,
+  .container-sm,
+  .container-md,
+  .container-lg,
+  .container-xl,
+  .container-xxl {
+    max-width: 1850px;
+  }
+}
+</style>
